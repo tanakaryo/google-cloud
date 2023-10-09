670792ef12630b9a17c17348e3bf23edfa45d29d
tanakaryo <yoichiikegawa@Yoichi-no-MacBook-Pro.local>
2023-10-10 02:18:12 +0900

    #26 add lifecycle resource.

---
+++ management/cloud-storage/lifecycle/test1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PRJ="nifty-gasket-399805"
+BKT_NAME="bkt-from-gcloud-jp-20231009"
+
+## create new bucket.
+# gcloud storage buckets create gs://${BKT_NAME} \
+# --project=${PRJ} \
+# --default-storage-class="COLDLINE" \
+# --location="asia-northeast1" \
+# --uniform-bucket-level-access
+gsutil mb \
+-p "${PRJ}" \
+-c "COLDLINE" \
+-l "asia-northeast1" \
+-b on gs://${BKT_NAME} 
+
+## add lifecycle-json
+# gcloud storage buckets update gs://${BKT_NAME} \
+# --lifecycle-file="./lifecycel/test1.json"
+gsutil lifecycle set ./lifecycle/test1.json gs://${BKT_NAME}
+
+## add object
+gsutil cp ./upload-items/test1.txt gs://${BKT_NAME}
+
+## delete bucket.
+#gcloud storage rm --recursive gs://${BKT_NAME}
+#gsutil rm -r gs://${BKT_NAME}
